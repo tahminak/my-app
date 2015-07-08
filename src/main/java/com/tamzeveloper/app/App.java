f97e101b3ad479c1c900b9d4eb3957a96e94b9a8
Tahmina Khan <tahmina.khan@gmail.com>
2015-07-08 13:35:21 -0400

    add new project

---
+++ src/main/java/com/tamzeveloper/app/App.java
@@ -0,0 +1,13 @@
+package com.tamzeveloper.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!"+"\nThis is first maven import" );
+    }
+}
